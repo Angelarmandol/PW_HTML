00a3f30c28d8030e8300357b5aade754d8e6b7d5
Angelarmandol <angelarmandol@hotmail.com>
2018-12-04 14:35:59 -0800

    add age list

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,27 @@
 //alert("Un mensaje de prueba");
-
+var edadString;
 function fn_btn_push(){
   var sexo = document.getElementsByName("sexo");
   var selectedGender;
+
+
 
   for(var i = 0; i < sexo.length; i++) {
     if(sexo[i].checked)
       selectedGender = sexo[i].value;
   }
 
+
   alert(selectedGender);
+
+
+}
+
+
+function lista(){
+  alert("entra");
+  var edad = document.getElementById("edad");
+  edadString =  edad.options[edad.selectedIndex].value.toString();
+  alert(edadString);
+
 }
