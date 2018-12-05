b828d1f176834c4ee8e4d92860fc32716059a1e1
Angelarmandol <angelarmandol@hotmail.com>
2018-12-05 13:27:13 -0800

    ready get data

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,32 @@
 //alert("Un mensaje de prueba");
 var edadString;
-function fn_btn_push(){
-  var sexo = document.getElementsByName("sexo");
-  var selectedGender;
+var sexo;
+var name;
+var apellido;
+var telefono;
+function fn_btn_push() {
+  sexo = document.getElementsByName("sexo")[0].value;
+  name = document.getElementsByName("nombre")[0].value;
+  apellido = document.getElementsByName("apellido")[0].value;
+  telefono = document.getElementsByName("telefono")[0].value;
 
 
-
-  for(var i = 0; i < sexo.length; i++) {
-    if(sexo[i].checked)
-      selectedGender = sexo[i].value;
-  }
-
-
-  alert(selectedGender);
+  alert("1");
+  alert(name);
+  alert("2");
+  alert(apellido);
+  alert("3");
+  alert(telefono);
+  alert("4");
+  alert(edadString);
+  alert("5");
+  alert(sexo);
 
 
 }
-
-
 function lista(){
   alert("entra");
   var edad = document.getElementById("edad");
   edadString =  edad.options[edad.selectedIndex].value.toString();
-  alert(edadString);
 
 }
